6d5b21e0d150aa2c766c2efa6ca01d173ce00199
prosenjit31 <prosenjitghosh3182000@gmail.com>
2020-08-09 19:58:00 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy
+//const name = "0" ;
+//0
+//""
+//null
+//undefined
+//NaN
+//truthy
+//'0','',[],{}
+let name =false;
+
+
+if(name ){
+   console.log("condition is true"); 
+}
+else{
+    console.log("condition is false");
+}
